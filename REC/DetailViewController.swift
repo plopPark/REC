f5fd51d2fa6653c6bc3c921bf984adf2cd9c9bcc
박첨벙 <114977536+plopPark@users.noreply.github.com>
2023-06-17 16:02:33 +0900

    feat : icon생성 함수 작성

--- REC/DetailViewController.swift
+++ REC/DetailViewController.swift
@@ -35,20 +35,8 @@ class DetailViewController: UIViewController {
             fetchMovieDetails(title: title)
             print("dndndndn \(title)")
         }
-        /*
-        movieTitle.text = getTitle
-        if let movies = loadMovies(), let movieTitle = getTitle {
-            if let movie = movies.first(where: { $0.title == movieTitle }) {
-                movieRate.text = String(movie.vote_average)
-                releaseDate.text = movie.release_date
-                runtime.text = String(movie.runtime)
-                genre.text = movie.genres.joined(separator: ", ")
-                overview.text = movie.overview
-                posterImage.image = UIImage(named: "poster")
-            }
-        }
-         */
-    }//디드로드 끝
+    }
+    //영화 정보 렌더링
     func fetchMovieDetails(title: String) {
         if let url = Bundle.main.url(forResource: "top100_movies", withExtension: "json") {
             do {
@@ -61,27 +49,22 @@ class DetailViewController: UIViewController {
                         //제목
                         self.movieTitle.text = movieDetail.title
                         //평점
-                        let attachment = NSTextAttachment()
-                        attachment.image = UIImage(systemName: "star.fill")
-                        let attachmentString = NSAttributedString(attachment: attachment)
-                        let completeText = NSMutableAttributedString()
-                        completeText.append(attachmentString)
-                        let movieRateText = NSAttributedString(string: " \(String(movieDetail.vote_average))")
-                        completeText.append(movieRateText)
-                        self.movieRate.text = completeText.string
-                        self.movieRate.attributedText = completeText
-
-                        //self.movieRate.text = String(movieDetail.vote_average)
-                        
+                        let movieRateText = self.symbolTextMaker(symbolName: "star.fill", text:String(movieDetail.vote_average))
+                        self.movieRate.attributedText = movieRateText
                         //개봉일
-                        self.releaseDate.text = movieDetail.release_date
+                        let realseDateText = self.symbolTextMaker(symbolName: "calendar.circle", text: String(movieDetail.release_date))
+                        self.releaseDate.attributedText = realseDateText
                         //런타임
-                        self.runtime.text = String(movieDetail.runtime ?? 0) + "분"
+                        let runtimeText = self.symbolTextMaker(symbolName: "clock", text: String("\(movieDetail.runtime)분"))
+                        self.runtime.attributedText = runtimeText
                         //장르
-                        self.genre.text = movieDetail.genres.joined(separator: ", ")
+                        let genreText = self.symbolTextMaker(symbolName: "popcorn.circle", text: movieDetail.genres.joined(separator: ", "))
+                        self.genre.attributedText = genreText
+
                         //줄거리
                         self.overview.text = movieDetail.overview
-                        //이미지
+                        
+                        //포스터
                         self.posterImage.image = UIImage(named: "poster")
                         print("UI should be updated now.")  // Debug print
                     }
@@ -95,4 +78,17 @@ class DetailViewController: UIViewController {
             print("JSON file not found")
         }
     }
+    //아이콘 추가
+    func symbolTextMaker(symbolName: String, text: String) -> NSAttributedString {
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: symbolName)
+        let attachmentString = NSAttributedString(attachment: attachment)
+        let completeText = NSMutableAttributedString()
+        completeText.append(attachmentString)
+        let text = NSAttributedString(string: " \(text)")
+        completeText.append(text)
+
+        return completeText
+    }
+
 }
